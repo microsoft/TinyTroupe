1669c6ef52b0d3dabc9f3621ccb447441bfb4658
mbreault <user.email=mbreault@users.noreply.github.com>
2025-03-18 00:20:42 -0400

    default until_done to False

--- tinytroupe/agent/tiny_person.py
+++ tinytroupe/agent/tiny_person.py
@@ -407,7 +407,7 @@ class TinyPerson(JsonSerializableRegistry):
     @transactional
     def act(
         self,
-        until_done=True,
+        until_done=False,
         n=None,
         return_actions=False,
         max_content_length=default["max_content_display_length"],
